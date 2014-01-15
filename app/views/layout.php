14725924b1031c9ec8cebfa4702112c938e92e50
Gregwar <g.passault@gmail.com>
2014-01-15 16:16:49 +0100

    Adding H1

--- app/views/layout.php
+++ app/views/layout.php
@@ -8,6 +8,7 @@
     </head>
     <body>
         <div class="page-header">
+            <h1>Plankton demo</h1>
             <nav class="navbar navbar-inverse" role="navigation">
                 <ul class="nav navbar-nav">
                     <li><a href="<?php echo path(); ?>">Home</a></li>
