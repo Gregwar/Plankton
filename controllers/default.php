3ee0bb1c0314d495f043ebd5607ec57fd350d51c
Gregwar <g.passault@gmail.com>
2014-01-15 13:39:38 +0100

    CS

--- controllers/default.php
+++ controllers/default.php
@@ -3,8 +3,9 @@
         return array('home');
     },
     '/hello' => function() {
-        return array('hello', array('name' => 
-            isset($_GET['name']) ? $_GET['name'] : '?'));
+        return array('hello', array(
+            'name' => isset($_GET['name']) ? $_GET['name'] : '?'
+        ));
     },
     '/404' => function() {
         return array('404');
