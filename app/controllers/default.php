4541f49f3f401e237c379aed1cd45659fd7f2c96
Gregwar <g.passault@gmail.com>
2014-01-20 11:17:48 +0100

    Not using http_response_code for PHP 5.3

--- app/controllers/default.php
+++ app/controllers/default.php
@@ -9,6 +9,8 @@
         ));
     },
     '/error' => function() {
+        header("HTTP/1.0 404 Not Found");
+        header("Status: 404 Not Found");
         http_response_code(404);    
         return array('error');
     }
