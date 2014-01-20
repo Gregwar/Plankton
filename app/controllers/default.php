d39b9492dde9f6a924eb434cb0bcd9cc7379367f
Gregwar <g.passault@gmail.com>
2014-01-20 11:24:33 +0100

    Removing http_response_code

--- app/controllers/default.php
+++ app/controllers/default.php
@@ -11,7 +11,6 @@
     '/error' => function() {
         header("HTTP/1.0 404 Not Found");
         header("Status: 404 Not Found");
-        http_response_code(404);    
         return array('error');
     }
 );
